bfda5e0ebc4bf48793fd5eb092a7de26c9c46ae5
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2025-04-20 17:49:17 +0530

    basic boilerplate setup

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+import bcrypt from 'bcryptjs'
+
+export const register = async ( req, res ) => {
+    const { email , password, name } = req.body
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const login = async ( req, res ) =>{}
+
+export const logout = async ( req, res ) =>{}
+
+export const check = async (req, res ) =>{}
