86c1c5b7c141d5fba39aea1f1f45dca048181691
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2025-04-20 18:27:02 +0530

    added register user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,15 +1,70 @@
 import bcrypt from 'bcryptjs'
+import { db } from '../libs/db.js'
+import { UserRole } from '../generated/prisma/index.js'
+import jwt from 'jsonwebtoken'
 
-export const register = async ( req, res ) => {
-    const { email , password, name } = req.body
+
+
+export const register = async (req, res) => {
+    const { email, password, name } = req.body;
+    
     try {
-        
-    } catch (error) {
-        
-    }
-}
+        const existingUser = await db.user.findUnique({
+            where: {
+                email
+            }
+        });
 
-export const login = async ( req, res ) =>{}
+        if (existingUser) {
+            return res.status(400).json({
+                error: "User already exists"
+            });
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10);
+        const newUser = await db.user.create({
+            data: {
+                email,
+                password: hashPassword,
+                name,
+                role: UserRole.USER
+            }
+        });
+
+        const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+            expiresIn: "7d"
+        });
+
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV !== "development",
+            maxAge: 1000 * 60 * 60 * 24 * 7
+        });
+
+        res.status(201).json({
+            message: "User created successfully",
+            user: {
+                id: newUser.id,
+                email: newUser.email,
+                name: newUser.name,
+                role: newUser.role,
+                image: newUser.image
+            }
+        });
+
+    } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).json({
+            error: "Error creating user"
+        });
+    }
+};
+
+
+export const login = async ( req, res ) =>{
+    
+}
 
 export const logout = async ( req, res ) =>{}
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
 const app = express()
 
-app.use(express.json())
-
+app.use(express.json());
+app.use(cookieParser());
 app.get("/", (req, res) =>{
     res.send("Hello Guys welcome to leetlab🤣")
 })
