71d3e945ef62307009c10dfa80ae8aec1b7eabf6
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2025-04-30 15:20:21 +0530

    Add batch submission and polling logic using Judge0 API

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,4 +1,32 @@
-export  const createProblem = async (req, res) => {}
+import db from '../libs/db.js'
+
+export  const createProblem = async (req, res) => {
+    const {title, description, difficulty, ags, examples, constraints, testcases, codeSnippet, referenceSolution } = req.body;
+
+    if(req.body.role !== "ADMIN"){
+        return res.status(403).json({error: "You are not allowed to create a problem"})
+    }
+    
+    try {
+        for(const [language, solutionCode] of Object.entries(referenceSolution)){
+            const languageId = getJudge0LanguageId(language)
+            if(!languageId) {
+                return res.status(400).json({error: `Language ${language} is not supported`})
+            }
+
+            const submissions = testcases.map((input, output) => ({
+                source_code: solutionCode,
+                language_id: languageId,
+                stdin: input,
+                expected_output: output
+            }))
+
+            const submissionResult = await submitBatch(submissions)
+        }
+    } catch (error) {
+        
+    }
+}
 export  const getAllProblems = async (req, res) => {}
 export  const getAllProblemById = async (req, res) => {}
 export  const updateProblem = async (req, res) => {}
