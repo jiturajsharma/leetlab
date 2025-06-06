1e5a6fd46a2248203a39d9c0ca2ffb668d6995cd
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2025-06-06 15:29:33 +0530

    problem databse desing and schema ready

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
 import cookieParser from 'cookie-parser'
 import problemRoutes from './routes/problem.routes.js'
+import executionRoute from './routes/executeCode.routes.js'
+
 
 dotenv.config()
 
@@ -16,6 +18,7 @@ app.get("/", (req, res) =>{
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/problems" , problemRoutes);
+app.use("/api/v1/execute-code" , executionRoute);
 
 
 app.listen(process.env.PORT, () => {
---
+++ src/routes/executeCode.routes.js
@@ -0,0 +1,6 @@
+import express from 'express'
+
+const executionRoute = express.Router();
+
+
+export default executionRoute;
